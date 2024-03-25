6533e6519f18337584053e207c1d79c35651882b
S-DeFerrari <66847874+S-DeFerrari@users.noreply.github.com>
2024-03-25 19:10:51 -0400

    Update ForkingHomeworkRfile.R
    adding in data.table methods

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(dplyr)
 library(data.table)
 
-diamonds
+data(diamonds)
 
 diamonds%>%
   select(carat,cut,price)%>%
@@ -10,3 +10,8 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+
+diamonds = as.data.table(diamonds)
+
+diamonds[cut == "Premium" & price > 7000 & price < 10000, .(carat, cut, price)][order(-carat)][1:20]
